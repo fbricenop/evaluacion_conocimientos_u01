b0d107afee7f2d2fcab07fd3542d736f2d644f09
Ferando <fbricenop@unjbg.edu.pe>
2024-06-23 19:32:00 -0500

    SEGUNDO COMMIT: AHORA SE PREGUNTARAN LOS NUMEROS UNA VEZ SELECCIONADA LA OPERACION

--- calculadora.cpp
+++ calculadora.cpp
@@ -8,23 +8,28 @@ int main(){
 	do{
 		cout<<"ESCRIBA EL NUMERO DE LA OPERACION A REALIZAR: "<<endl<<"0)SALIR \n1)SUMA \n2)RESTA \n3)MULTIPLICACION \n4)DIVISION"<<endl;
 		cin>>op;
-		cout<<"INGRESE LOS DOS NUMEROS: "<<endl;
-		cin>>n1>>n2;
 		switch(op){
 			case 1:{
+				cout<<"INGRESE LOS DOS NUMEROS: "<<endl;
+				cin>>n1>>n2;
 				cout<<"LA SUMA DE LOS DOS NUMEROS ES: "<<endl<<n1+n2<<endl;
 				break;
 			}
 			case 2:{
+				cout<<"INGRESE LOS DOS NUMEROS: "<<endl;
+				cin>>n1>>n2;
 				cout<<"LA RESTA DE LOS DOS NUMEROS ES: "<<endl<<n1-n2<<endl;
 				break;
 			}
 			case 3:{
+				cout<<"INGRESE LOS DOS NUMEROS: "<<endl;
+				cin>>n1>>n2;
 				cout<<"LA MULTIPLICACION DE LOS DOS NUMEROS ES: "<<endl<<n1*n2<<endl;
 				break;
 			}
 			case 4:{
-				
+				cout<<"INGRESE LOS DOS NUMEROS: "<<endl;
+				cin>>n1>>n2;
 				cout<<"LA DIVISION DE LOS DOS NUMEROS ES: "<<endl<<n1/n2<<endl;
 				break;
 			}
